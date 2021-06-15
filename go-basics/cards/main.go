91a609f3745ad219bca3ffb285c48a209c2c76fa
Ivo Costa <ivo.costa@creativecapsule.com>
2021-06-16 00:51:40 +0530

    add custom type

--- go-basics/cards/main.go
+++ go-basics/cards/main.go
@@ -1,18 +1,10 @@
 package main
 
-import "fmt"
-
-var deckSize int
-
 func main() {
-	cards := []string{newCard(), "Ace of Diamonds"}
+	cards := deck{newCard(), "Ace of Diamonds"}
 	cards = append(cards, "Six of Spades")
-	deckSize = 55
-	fmt.Println(deckSize)
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
 
+	cards.print()
 }
 
 func newCard() string {
