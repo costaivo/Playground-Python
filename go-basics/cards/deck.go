1dc95ea12f290645dfffbc78c676846df8312076
Costa Ivo <costa_ivo@yahoo.com>
2021-06-22 21:26:33 +0530

    added full deck cards
    Signed-off-by: Costa Ivo <costa_ivo@yahoo.com>

--- go-basics/cards/deck.go
+++ go-basics/cards/deck.go
@@ -9,7 +9,7 @@ type deck []string
 func newDeck() deck {
 	cards := deck{}
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
@@ -18,13 +18,13 @@ func newDeck() deck {
 	}
 	return cards
 }
+
 func (d deck) print() {
 	fmt.Println("**********")
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
 	fmt.Println("**********")
-
 }
 
 func deal(d deck, handSize int) (deck, deck) {
