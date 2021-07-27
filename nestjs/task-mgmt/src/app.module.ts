018d3ef9f49ac35cf2154d14cae19deeb82f8a02
Ivo Costa <costaivo@users.noreply.github.com>
2021-07-27 23:31:06 +0530

    fixed configuration

--- nestjs/task-mgmt/src/app.module.ts
+++ nestjs/task-mgmt/src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { configValidationSchema } from './config.schema';
+import { getTypeOrmConfiguration } from './typeorm.config';
 
 @Module({
   imports: [
@@ -15,16 +16,7 @@ import { configValidationSchema } from './config.schema';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        autoLoadEntities: true,
-        synchronize: true,
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_DATABASE'),
-      }),
+      useFactory: async (configService: ConfigService) => getTypeOrmConfiguration(configService),
     }),
     AuthModule,
   ],
---
+++ nestjs/task-mgmt/src/typeorm.config.ts
@@ -0,0 +1,13 @@
+import { ConfigService } from '@nestjs/config';
+import {TypeOrmModuleOptions} from '@nestjs/typeorm';
+
+export const getTypeOrmConfiguration = async (configService: ConfigService):Promise<TypeOrmModuleOptions> => ({
+    type: 'postgres',
+    autoLoadEntities: true,
+    synchronize: true,
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_DATABASE'),
+  })
