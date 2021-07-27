194d9cd58a43c515b579ffb2b97ddb5214f5096d
Ivo Costa <costaivo@users.noreply.github.com>
2021-07-27 23:12:45 +0530

    fix: env bug

--- nestjs/task-mgmt/src/app.module.ts
+++ nestjs/task-mgmt/src/app.module.ts
@@ -1,23 +1,21 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { configValidationSchema } from './config.schema';
-
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [`.env.stage.${process.env.STAGE}`],
-      validationSchema: configValidationSchema
+      validationSchema: configValidationSchema,
     }),
     TasksModule,
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => (
-        {
+      useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
         autoLoadEntities: true,
         synchronize: true,
@@ -28,9 +26,7 @@ import { configValidationSchema } from './config.schema';
         database: configService.get('DB_DATABASE'),
       }),
     }),
-    AuthModule
+    AuthModule,
   ],
 })
-export class AppModule { }
-
-
+export class AppModule {}
